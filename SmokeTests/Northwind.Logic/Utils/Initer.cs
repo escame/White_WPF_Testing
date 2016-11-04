bf108d8f5ce3f1f4fceb757533a7702e2fab5034
escame <escame@hotmail.com>
2016-11-04 12:56:45 -0700

    Adding Sample Northwind WPF solution

---
+++ SmokeTests/Northwind.Logic/Utils/Initer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Northwind.Logic.Utils
+{
+    public static class Initer
+    {
+        public static void Init()
+        {
+            SessionFactory.Init(@"Server=.\SQLEXPRESS;Database=Northwind;Trusted_Connection=true");
+        }
+    }
+}
